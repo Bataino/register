a8f0b985f793f4d8b7577a4c508135823b3e406a
bataino <bataino.ronaldo@gmail.com>
2021-06-11 20:12:16 +0100

    latest

--- script.js
+++ script.js
@@ -2,8 +2,14 @@ var sign_in = document.getElementById('sign-in');
 var formBody = document.getElementById('form-body');
 var sign_in_office = document.getElementById('sign-in-office');
 
-
 sign_in.addEventListener('click', function displayFunction(){
+    sign_in.style.display ='none'
+    sign_in_office.style.display ='none'
+    formBody.style.display = 'block'
+}
+);
+
+sign_in_office.addEventListener('click', function show(){
     sign_in.style.display ='none'
     sign_in_office.style.display ='none'
     formBody.style.display = 'block'
